65bd75e7b11a50c194976e397f3824cd9f415e38
Nostyone <peter.genchev@yahoo.bg>
2023-11-19 08:52:55 +0200

    fix  auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,53 +1,27 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { signOut } from '../../firebase'; // Adjust the import path based on your file structure
+import { initializeApp } from 'firebase/app';
+import { getAuth, signOut } from 'firebase/auth';
 
-function Navigation() {
-  const [user] = useAuthState(auth);
+const firebaseConfig = {
+  apiKey: "AIzaSyDHhNPOFQz-HlB8QcRFU2ZryFO8XafBhJg",
+  authDomain: "reactapp-db960.firebaseapp.com",
+  projectId: "reactapp-db960",
+  storageBucket: "reactapp-db960.appspot.com",
+  messagingSenderId: "368264839083",
+  appId: "1:368264839083:web:dc3b1ced01f6a9f5565cee"
+};
 
-  const handleLogout = async () => {
-    try {
-      await signOut(auth);
-      // You may want to redirect the user to the home page or another page after logout
-    } catch (error) {
-      console.error('Logout error:', error.message);
-    }
-  };
+export const registerUser = async (email, password) => {
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    await signInWithEmailAndPassword(auth, email, password);
+    console.log('Registration successful');
+  } catch (error) {
+    console.error('Registration error:', error.message);
+  }
+};
 
-  return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand as={Link} to="/">CarMarket</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-            {/* Conditional rendering based on user authentication */}
-            {user ? (
-              <>
-                <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
-                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-                <Nav.Link as={Link} to="/catalog">Catalog</Nav.Link>
-              </>
-            ) : (
-              <>
-                <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                <Nav.Link as={Link} to="/register">Register</Nav.Link>
-              </>
-            )}
-            {/* End of conditional rendering */}
-            
-            <Nav.Link as={Link} to="/addCar">Add Car</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
-
-export default Navigation;
+export { auth, getAuth, signOut }; // Export signOut along with other necessary exports
+export default app;
